6e955642452fc5e763c3680775ead41b4249b5bc
JacquelineJia <jackiesisy@gmail.com>
2017-09-19 12:30:15 -0400

    black rectangle

--- planet-trouble.js
+++ planet-trouble.js
@@ -75,8 +75,8 @@ const ctx = canvas.getContext('2d');
 
 function game() {
   ctx.clearRect(0, 0, 150, 75);
-  ctx.fillStyle = "#FF0000";
-  ctx.fillRect(0,0,150,75);
+  ctx.fillStyle = "#222222";
+  ctx.fillRect(0,0,832,512);
 
   window.requestAnimationFrame(game);
 }
