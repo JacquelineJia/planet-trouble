590fd4a2ef5c146d9eee9c5778b785ffb1c43ccd
JacquelineJia <jackiesisy@gmail.com>
2017-09-19 12:34:19 -0400

    changed rectangle to canvas width and height

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const ctx = canvas.getContext('2d');
 function game() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = "#222222";
-  ctx.fillRect(0,0,832,512);
+  ctx.fillRect(0,0,canvas.width,canvas.height);
 
   window.requestAnimationFrame(game);
 }
