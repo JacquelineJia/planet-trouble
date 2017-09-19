5a9314d5430dc59439b44043427773e1da25d972
Cameron Eldridge <cameldridge+git@gmail.com>
2017-09-19 12:33:28 -0400

    Add a player file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import { drawPlayer } from './player';
+
 const canvas = document.querySelector('#game');
 const ctx = canvas.getContext('2d');
 
@@ -7,6 +9,8 @@ function game() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = "#222222";
   ctx.fillRect(0,0,832,512);
+
+  drawPlayer();
 
   window.requestAnimationFrame(game);
 }
